3e98398220d07ca64a800141a4afdb6be8b3563f
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-12-23 14:32:27 -0600

    Reworked canvas split between grids Moved border creation to Canvas class. coordinates for border between grids is passed as x,y values

--- Canvas.js
+++ Canvas.js
@@ -1,5 +1,4 @@
 import Grid from "./Grid.js";
-import Rules from "./Rules.js";
 
 export default class Canvas {
     constructor(ctx, cellSize=1, borderSize=1, grids=1, seed, pattern="square"){
@@ -7,31 +6,34 @@ export default class Canvas {
         this.gridPattern = pattern;
         this.gridCount = grids;
         this.borderSize = borderSize;
+        this.borderCoords = [];
+        this.borderCoords.push([0,0,ctx.canvas.width, ctx.canvas.height]);
         this.grids = this.#splitCanvas(ctx, seed);
+        this.#drawBorder(ctx);
+        this.grids.forEach(grid => grid.wipeField());
     }
     #splitCanvas(ctx, seed){
         let temp = [];
-        const calcCellGrid = (dval, val) => (dval-val)/this.cellSize;
-        switch(this.gridPattern){
-            default: {
-                const gridWidth = (ctx.canvas.width/this.gridCount) - (2*this.borderSize);
-                const gridHeight = ctx.canvas.height - (2*this.borderSize);
-                const y = this.borderSize;
-                const dy = y+gridHeight;
-                let dx = (x) => x+gridWidth;
-                for(let i=0; i < this.gridCount; i++){
-                    let xval = i*gridWidth+this.borderSize;
-                    let dxval = dx(xval);
-                    temp.push(new Grid(ctx, {
-                        x: xval,
-                        y: y, 
-                        dx: dxval, 
-                        dy: dy,
-                        size: this.cellSize
-                    }, calcCellGrid(dxval, xval), calcCellGrid(dy,y), seed));
-                }
-            }
+        const gridWidth = (ctx.canvas.width/this.gridCount) - 2*this.borderSize;
+        const gridHeight = ctx.canvas.height - 2*this.borderSize;
+        const initialX = (inc) => this.borderSize+(inc*gridWidth)+(inc*this.borderSize*2);
+        for(let i = 0; i < this.gridCount; i++){
+            let coords = {};
+            coords.x = initialX(i);
+            coords.y = this.borderSize;
+            coords.dx = coords.x + gridWidth - coords.x - this.borderSize;
+            coords.dy = coords.y + gridHeight - this.borderSize;
+            coords.size = this.cellSize;
+            temp.push(new Grid(ctx, coords, coords.dx-coords.x, coords.dy-coords.y, seed));
         }
         return temp;
+    }
+    #drawBorder(ctx){
+        this.borderCoords.forEach(coord => {
+            //console.log(coord);
+            ctx.beginPath();
+            ctx.fillStyle = "purple";
+            ctx.fillRect(...coord);
+        })
     }
 }
