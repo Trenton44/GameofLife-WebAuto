f611c5cdfee3650c9b9b17c059d61b6407822aa9
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-12-23 17:37:43 -0600

    Simplified draw function, moved pixel changes to cell.draw() updated index.js for testing

--- Grid.js
+++ Grid.js
@@ -1,11 +1,12 @@
+import Cell from "./Cell.js";
 import Rules from "./Rules.js";
-const genColor = (min=150, max=255) => Math.floor(Math.random() * (max-min+1)) + min;
+
 export default class Grid {
     constructor(ctx, ctxSpace, width, height, seed){
         this.ctx = ctx;
-        this.ctxSpace  = ctxSpace;
-        this.width  = width/(ctxSpace.size*ctxSpace.size);
-        this.height  = height/(ctxSpace.size*ctxSpace.size);
+        this.ctxSpace = ctxSpace;
+        this.width = width/(this.ctxSpace.size*this.ctxSpace.size);
+        this.height = height/(this.ctxSpace.size*this.ctxSpace.size);
         this.grid = this.#newGame(seed);
     }
     #newGame(seed){
@@ -14,20 +15,19 @@ export default class Grid {
             throw Error("Seed should be at least two integers long.");
         for(let x = 0; x < this.width; x++){
             grid[x] = [];
-            for(let y=0; y < this.height; y++){
+            for(let y = 0; y < this.height; y++){
                 let alive = x % seed[0] == 0 && y % seed[1] == 0;
-                grid[x][y] = alive;
+                grid[x][y] = new Cell(alive, x, y, this.ctxSpace.size, this.width);
             }
         }
         return grid;
     }
     #newGeneration(){
-        let grid = [];
-        this.grid.forEach((yarr, x) => {
-            grid[x] = [];
-            yarr.forEach((cell, y) => grid[x][y] = Rules(cell, this.getNeighbors(x,y)));
-        });
-        return grid;
+        let temp = [...this.grid];
+        temp.forEach((yarr, x) => 
+            yarr.forEach((cell, y) => cell.alive = Rules(cell.alive, this.getNeighbors(x, y)))
+        );
+        return temp;
     }
     getNeighbors(x, y){
         let temp = [];
@@ -36,7 +36,7 @@ export default class Grid {
             for(let z=y-1; z < y+2; z++){
                 if(z < 0 || z>= this.height){ continue; }
                 if(i == x && z == y){ continue; }
-                temp.push(this.grid[i][z]);
+                temp.push(this.grid[i][z].alive);
             }
         }
         return temp;
@@ -45,31 +45,9 @@ export default class Grid {
         let temp = this.#newGeneration();
         let imageData = this.ctx.getImageData(this.ctxSpace.x, this.ctxSpace.y, this.ctxSpace.dx, this.ctxSpace.dy);
         let next = this.ctx.createImageData(imageData);
-        let pixels = [];
-        temp.forEach((yarr, xindex) => {
-            yarr.forEach((cell, yindex) => {
-                let pixelColor = cell ? [genColor(), genColor(), genColor(), 255] : [0, 0, 0, 255];
-                for(let i=xindex; i<xindex+this.ctxSpace.size; i++){
-                    if(!pixels[i]){ pixels[i] = []; }
-                    for(let z=yindex; z<yindex+this.ctxSpace.size; z++){
-                        pixels[i][z] = pixelColor;
-                        //let location = (xindex+i)*(yarr.length*4) + (yindex+z)*4;
-                        //console.log([next.data[location], next.data[location+1], next.data[location+2], next.data[location+3]])
-                        // go to every pixel location (coords are for ctx.ImageData). change each of the 4 pixels to the new color
-                        //console.log("original: "+next.data[pixelLoc]+","+next.data[pixelLoc+1]+","+next.data[pixelLoc+2]+","+next.data[pixelLoc+3]+",");
-                        //for(let j=0; j<4; j++){ next.data[location+j] = pixelColor[j]; }
-                        //console.log("altered: "+next.data[pixelLoc]+","+next.data[pixelLoc+1]+","+next.data[pixelLoc+2]+","+next.data[pixelLoc+3]+",");
-                    }
-                }
-            });
-        });
-        pixels = pixels.flat();
-        pixels.forEach((pixel, index) => {
-            next.data[index] = pixel[0];
-            next.data[index+1] = pixel[1];
-            next.data[index+2] = pixel[2];
-            next.data[index+3] = pixel[3];
-        });
+        temp.forEach((yarr, xindex) => 
+            yarr.forEach((cell, yindex) => cell.draw(next.data, temp.length, yarr.length))
+        );
         this.ctx.putImageData(next, this.ctxSpace.x, this.ctxSpace.y);
         this.grid = temp;
     }
--- index.js
+++ index.js
@@ -6,16 +6,17 @@ async function start(seed){
     const canvas = document.createElement("canvas");
     const root = document.getElementById("root");
     root.appendChild(canvas);
-    canvas.width = 800;
-    canvas.height = 800;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
     const ctx = canvas.getContext("2d");
-    const canvasLogic = new Canvas(ctx, 1, 1, 1, seed);
+    const canvasLogic = new Canvas(ctx, 1, 2, 1, seed);
     console.log(canvasLogic.grids);
-    //canvasLogic.grids.forEach(grid => grid.drawBorder());
-    
-    while(true){
+    canvasLogic.grids.forEach(grid => grid.draw());
+    /*while(true){
+        console.log("rendering");
         canvasLogic.grids.forEach(grid => grid.draw());
         await sleep(500);
-    }
+    }*/
 }
+console.log("loading canvas.");
 start("22");
