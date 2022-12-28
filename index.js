f1e74bbe35923593d19add7126f16c204f602b99
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-12-28 09:04:56 -0600

    Changed canvas to 400x400 to conserve memory

--- index.js
+++ index.js
@@ -6,17 +6,15 @@ async function start(seed){
     const canvas = document.createElement("canvas");
     const root = document.getElementById("root");
     root.appendChild(canvas);
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    canvas.width = 400;
+    canvas.height = 400;
     const ctx = canvas.getContext("2d");
-    const canvasLogic = new Canvas(ctx, 1, 2, 1, seed);
+    const canvasLogic = new Canvas(ctx, 1, 2, 2, seed);
     console.log(canvasLogic.grids);
-    canvasLogic.grids.forEach(grid => grid.draw());
-    /*while(true){
-        console.log("rendering");
+    while(true){
         canvasLogic.grids.forEach(grid => grid.draw());
-        await sleep(500);
-    }*/
+        await sleep(sleepTime);
+    }
 }
 console.log("loading canvas.");
 start("22");
