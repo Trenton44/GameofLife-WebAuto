76fddc4adfd27b2deba6a8b1e26ce02711ecc25e
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-12-28 09:04:35 -0600

    Fixed canvasPosition function to include *4 for pixels -Added color check into draw(), now correctly changes color on state

--- Cell.js
+++ Cell.js
@@ -1,9 +1,10 @@
 const genColor = (min=150, max=255) => Math.floor(Math.random() * (max-min+1)) + min;
+const Black = [0, 0, 0, 255];
 const pixelPositions = (x, y, scalar, width) => {
     let temp = [];
     for(let i=x; i<x+scalar; i++){
         for(let z=y; z<y+scalar; z++)
-        { temp.push(z*width+i); }
+        { temp.push((z*width+i)*4); }
     }
     return temp;
 };
@@ -13,15 +14,17 @@ export default class Cell {
         this.scalar = pixelScalar;
         this.gridPosition = [x, y];
         this.canvasPositions = pixelPositions(...this.gridPosition, this.scalar, gridW);
-        this.color = state ? [genColor(), genColor(), genColor(), 255] : [0, 0, 0, 255];
-        
+        this.color = [genColor(), genColor(), genColor(), 255];
     }
+    set alive(value){ this.state = value; }
+    get alive(){ return this.state; }
     draw(arr){
+        let color = this.state ? this.color : Black;
         this.canvasPositions.forEach(pos => {
-            arr[pos] = this.color[0];
-            arr[pos+1] = this.color[1];
-            arr[pos+2] = this.color[2];
-            arr[pos+3] = this.color[3];
+            arr[pos] = color[0];
+            arr[pos+1] = color[1];
+            arr[pos+2] = color[2];
+            arr[pos+3] = color[3];
         })
     }
 }
