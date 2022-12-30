84575e2e60ebac5849a3f6d249ef7ae251de7a57
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-12-29 23:25:28 -0600

    updated index.js to match

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import Canvas from "./Canvas.js";
 const sleep = ms => new Promise(r => setTimeout(r, ms));
-const sleepTime = 16;
+const sleepTime = 64;
 
 async function start(seed){
     const canvas = document.createElement("canvas");
     const root = document.getElementById("root");
     root.appendChild(canvas);
-    canvas.width = 400;
-    canvas.height = 400;
+    canvas.width = 802;
+    canvas.height = 802;
     const ctx = canvas.getContext("2d");
-    const canvasLogic = new Canvas(ctx, 1, 2, 2, seed);
+    const canvasLogic = new Canvas(ctx, 1, 2, 1, seed);
     console.log(canvasLogic.grids);
+    canvasLogic.grids.forEach(grid => grid.draw(grid.grid));
     while(true){
-        canvasLogic.grids.forEach(grid => grid.draw());
+        canvasLogic.grids.forEach(grid => grid.age());
         await sleep(sleepTime);
     }
 }
