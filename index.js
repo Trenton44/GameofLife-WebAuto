4edfeec385c1ca9b842f92b5493c826fd5e09dda
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-12-30 11:17:37 -0600

    getContext now set to "willReadFrequently"

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Canvas from "./Canvas.js";
 const sleep = ms => new Promise(r => setTimeout(r, ms));
-const sleepTime = 64;
+const sleepTime = 640;
 
 async function start(seed){
     const canvas = document.createElement("canvas");
@@ -8,10 +8,11 @@ async function start(seed){
     root.appendChild(canvas);
     canvas.width = 802;
     canvas.height = 802;
-    const ctx = canvas.getContext("2d");
-    const canvasLogic = new Canvas(ctx, 1, 2, 1, seed);
+    const ctx = canvas.getContext("2d", { willReadFrequently: true });
+    const canvasLogic = new Canvas(ctx, 2, 2, 1, seed);
     console.log(canvasLogic.grids);
     canvasLogic.grids.forEach(grid => grid.draw(grid.grid));
+    await sleep(500);
     while(true){
         canvasLogic.grids.forEach(grid => grid.age());
         await sleep(sleepTime);
