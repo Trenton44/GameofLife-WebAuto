8c7e723301135c0b3fb951416b9557e955739296
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2023-01-03 23:04:46 -0600

    Updated Canvas class -takes current width/height and rounds to nearest hundredth -calculates aspect ratio of canvas for splitting grids

--- Canvas.js
+++ Canvas.js
@@ -1,23 +1,46 @@
 import Grid from "./Grid.js";
-
+const GCD = (a, b) => b == 0 ? a : GCD(b, a%b);
 export default class Canvas {
-    constructor(ctx, cellSize=1, borderSize=1, grids=1, seed, pattern="square"){
-        this.cellSize = cellSize;
-        this.gridPattern = pattern;
-        this.gridCount = grids;
-        this.borderSize = borderSize;
-        this.borderCoords = [];
-        this.borderCoords.push([0, 0, ctx.canvas.width, ctx.canvas.height]);
+    constructor(ctx, borderSize=1, grids=1){
+        // round the canvas to the nearest hundredth
+        ctx.canvas.width = Math.floor(ctx.canvas.width/100)*100;
+        ctx.canvas.height = Math.floor(ctx.canvas.height/100)*100;
+        let gcd = GCD(ctx.canvas.width, ctx.canvas.height);
+        this.ratio = [ctx.canvas.width/gcd, ctx.canvas.height/gcd];
+        console.log(this.ratio);
+        this.borderCoords = [[0, 0, ctx.canvas.width, ctx.canvas.height]];
         console.log("setting up Grids.");
-        this.grids = this.#splitCanvas(ctx, seed);
+        //this.grids = this.#splitCanvas(ctx, borderSize, grids);
         console.log("Grid setup complete.");
         console.log("Drawing borders.");
-        this.#drawBorder(ctx);
+        this.borderCoords.forEach(coord => {
+            ctx.beginPath();
+            ctx.fillStyle = "purple";
+            ctx.fillRect(...coord);
+        });
         console.log("Borders drawn. drawing Grid spaces...");
-        this.grids.forEach(grid => grid.wipeField());
+        //this.grids.forEach(grid => grid.wipeField());
         console.log("Grid spaces drawn.");
+        console.log(ctx);
     }
-    #splitCanvas(ctx, seed){
+    #splitCanvas(ctx, borderSize, grids){
+        const gridWidth = (ctx.canvas.width/grids) - borderSize;
+        const gridHeight = ctx.canvas.height - 2*borderSize;
+        const initialX = (inc) => borderSize+(inc*gridWidth)+(inc*borderSize);
+        //calculate aspect ratio of canvas. 4:3, 16:9, etc.
+        // this becomes the values of the for loops
+        // (Every 4 grids, make a new row)
+        for(let i=0; i < grids; i++){
+            console.log("Setting up Grid "+i);
+            let coords = {};
+
+        }
+        return {};
+    }
+}
+
+/**
+ * #splitCanvas(ctx, seed){
         let temp = [];
         const gridWidth = (ctx.canvas.width/this.gridCount) - this.borderSize;
         const gridHeight = ctx.canvas.height - 2*this.borderSize;
@@ -36,11 +59,4 @@ export default class Canvas {
         }
         return temp;
     }
-    #drawBorder(ctx){
-        this.borderCoords.forEach(coord => {
-            ctx.beginPath();
-            ctx.fillStyle = "purple";
-            ctx.fillRect(...coord);
-        })
-    }
-}
+ */
--- index.js
+++ index.js
@@ -6,17 +6,17 @@ async function start(seed){
     const canvas = document.createElement("canvas");
     const root = document.getElementById("root");
     root.appendChild(canvas);
-    canvas.width = 802;
-    canvas.height = 802;
+    canvas.width = 955;
+    canvas.height = 634;
     const ctx = canvas.getContext("2d", { willReadFrequently: true });
-    const canvasLogic = new Canvas(ctx, 2, 1, 1, seed);
+    const canvasLogic = new Canvas(ctx, 12, 1, 1, seed);
     console.log(canvasLogic.grids);
-    canvasLogic.grids.forEach(grid => grid.draw(grid.grid));
+    /*canvasLogic.grids.forEach(grid => grid.draw(grid.grid));
     await sleep(500);
     while(true){
         canvasLogic.grids.forEach(grid => grid.age());
         await sleep(sleepTime);
-    }
+    }*/
 }
 console.log("loading canvas.");
 start("22");
