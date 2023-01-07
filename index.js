1d129031ff8235e082a191386037461aeed87478
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2023-01-06 23:42:30 -0600

    Added initial code for sidebar

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ const sleepTime = 64;
 
 async function start(seed){
     const canvas = document.createElement("canvas");
+    canvas.id = "gol-canvas";
+    document.onmouseup = (event) => {
+        console.log(event.target.id);
+        if(event.target.id !== "gol-canvas")
+            document.getElementById("canvas-context-menu").style.visibility = "hidden";
+    }
     const root = document.getElementById("root");
     root.appendChild(canvas);
-    canvas.width = 955;
-    canvas.height = 634;
-    const ctx = canvas.getContext("2d", { willReadFrequently: true });
-    const canvasLogic = new Canvas(ctx, 12, 1, 1, seed);
+    canvas.width = 800;
+    canvas.height = 800;
+    const canvasLogic = new Canvas(canvas, 12, 1, 1, seed);
     console.log(canvasLogic.grids);
+    canvasLogic.grids.forEach(grid => grid.init());
+    canvasLogic.SubdivideGrid(0);
+    let colors = ["red", "green", "blue", "yellow"];
+    canvasLogic.grids.forEach((grid, index) => grid.wipeField(colors[index]));
     /*canvasLogic.grids.forEach(grid => grid.draw(grid.grid));
     await sleep(500);
     while(true){
