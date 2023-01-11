2a0649eb73174997d50ce1512fde22973323405d
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2023-01-10 18:19:59 -0600

    Added JSDocumentation to Canvas Class

--- Canvas.js
+++ Canvas.js
@@ -1,43 +1,67 @@
 import Grid from "./Grid.js";
 
+/**
+ * Returns an integer rounded down to the nearest "place" digit (100 = hundredths place, 10= tenths place... etc.)
+ * @param {integer} num 
+ * @param {integer} place 
+ * @returns integer
+ */
 const Round = (num, place) => Math.floor(num/place)*place;
+
+/** Class representing an html canvas */
 export default class Canvas {
-    constructor(canvas){
+    /**
+     * Create an instance of Canvas
+     * @param {HTMLCanvasElement} canvas - the canvas element on the page
+     * @param {HTMLDivElement} overlay - The div element that will display canvas statistics
+     * @returns {boolean}
+     */
+    constructor(canvas, overlay){
         // round the canvas to the nearest hundredth
         this.canvas = canvas;
+        this.overlay = overlay;
         this.ctx = this.canvas.getContext("2d", { willReadFrequently: true });
-        // Need to .bind() the class instance as "this".
         this.setupHandlers();
+        // Rounds width and height to avoid overlapping of cells on pixels
         this.ctx.canvas.width = Round(this.ctx.canvas.width, 100);
         this.ctx.canvas.height = Round(this.ctx.canvas.height, 100);
         console.log("setting up Grids.");
-        // grid needs the cell size to be auto-calculated.
+        // create the initial grid, using the entire canvas
         this.grids = [new Grid(this.ctx, {
             x: 0,
             y: 0,
             dx: this.ctx.canvas.width,
             dy: this.ctx.canvas.height
         })];
+        // Initialize the first GoL grid with cell data.
         this.grids[0].init();
         console.log("Grid setup complete.");
-        console.log("Drawing borders.");
-        console.log("Borders drawn. drawing Grid spaces...");
         this.grids.forEach(grid => grid.wipeField());
         console.log("Grid spaces drawn.");
-        console.log(this.ctx);
+        return true
     }
+    /**
+     * Sets up event listeners for the canvas element to respond to user clicks
+     * Current Events: 
+     *  -OnMouseUp: select grid
+     *  -OnMouseMove: apply orange hue to moused over grid
+     *  -OnMouseOut: remove all hover effects from grids, to avoid last hovered grid retaining effect.
+     */
     setupHandlers(){
+        // User click should cause the canvas's overlay div to appear, and populate it with the selected grid's information
         this.canvas.onmouseup = (event) => {
-            let contextMenu = document.getElementById("canvas-context-menu");
             let gridI = this.findGrid(event.x, event.y);
-            if(gridI == null)
-                contextMenu.style.visibility = "hidden";
-            contextMenu.children[0].innerHTML = "Square "+gridI+": "+this.grids[gridI].ctxSpace;
-            contextMenu.style.visibility = "visible";
+            if(gridI == null){
+                this.overlay.style.visibility = "hidden";
+                return false;
+            }
+            this.grids[gridI].displayData(this.overlay);
+            this.overlay.style.visibility = "visible";
             return true;
         };
+        // When the user hovers over the canvas with the mouse, should remove the find the grid hovered over, and apply an effect to the cells in this grid to represent the hover visually
         this.canvas.onmousemove = (event) => {
-            // wipe hover effect from all grids, because we have to recheck which grid mouse is on with each even.
+            // wipe hover effect from all grids, because we have to recheck which grid mouse is on with each event.
             this.grids.forEach(grid => delete grid.effects.hover);
             const OrangeHue = (pixels, inds) => {
                 pixels[inds[0]] = pixels[inds[0]] + (255 - pixels[inds[0]])/2;
@@ -46,13 +70,18 @@ export default class Canvas {
             let gridI = this.findGrid(event.x, event.y);
             if(gridI == null)
                 return null;
+            // applies an orange effect to the pixel array in the affected grid.
             this.grids[gridI].effects.hover = OrangeHue;
-            // add orange effect here.
         };
-        this.canvas.onmouseout = (event) => {
-            this.grids.forEach(grid => delete grid.effects.hover);
-        };
+        // Moving the mouse off of the canvas should wipe the hover effect from the grids
+        this.canvas.onmouseout = (event) => this.grids.forEach(grid => delete grid.effects.hover);
     }
+    /**
+     * Subdivides the selected grid into 4 seperate Grid (GoL) instances, retaining the existing cells within that divided section.
+     * Currently limited to only 4, to preserve aspect ratio (and by extension, game state, as Cell count/pos is currently tied to grid size/ratio).
+     * @param {integer} index - the location in this.grids of the grid to be sudivided within the Canvas
+     * @returns true
+     */
     SubdivideGrid(index){
         let grids = [];
         let current = this.grids[index];
@@ -74,6 +103,12 @@ export default class Canvas {
         }
         this.grids.splice(index, 1, ...grids);
     }
+    /**
+     * 
+     * @param {integer} x - The x-coordinate of the mouse, relative to the inside of the Canvas element
+     * @param {integer} y - The y-coordinate of the mouse, relative to the inside of the Canvas element
+     * @returns {integer} - returns index of grid within Canvas.grids, or null if no grid is found
+     */
     findGrid(x, y){
         for(let index in this.grids){
             let coords = this.grids[index].ctxSpace;
